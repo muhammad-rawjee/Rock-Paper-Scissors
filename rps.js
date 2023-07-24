199915fd00df66c24ede82c62a8e3dafb0a30c2c
muhammad-rawjee <muhammadrawjee@gmail.com>
2023-07-24 18:36:52 -0400

    Almost Done

--- rps.js
+++ rps.js
@@ -1,3 +1,25 @@
+// 
+const buttons = document.querySelectorAll('.choice > #btn');
+const currRound = document.querySelector('.res-cont > .curr-round');
+const finalRes = document.querySelector('.res-cont > .final-res');
+const PScore = document.querySelector('.human-cont > .info > .stats > .score');
+const CScore = document.querySelector('.comp-cont > .info > .stats > .score');
+const tryAgain = document.querySelector('.res-cont > .try-again')
+console.log(PScore);
+let playerScore = 0;
+let computerScore = 0;
+
+// Fin
+let disableButtons = () => {
+    buttons.forEach(button => {
+        button.disabled = true
+    })
+}
+
+tryAgain.addEventListener('click', () => {
+    location.reload();
+});
+// Fin
 let getComputerChoice = () => {
     let randNum = Math.floor(Math.random() * (3) + 1);
     if (randNum == 1){
@@ -11,53 +33,54 @@ let getComputerChoice = () => {
     }
 }
 
-let playRound = (playerSelection, computerSelection) => {
-    playerSelection = playerSelection.toLowerCase()
-    computerSelection = computerSelection.toLowerCase()
+// Fin
+let determineWinner = (user, computer) => {
+    if (user === computer) {
+        return "It's a tie!";
+    } else if (
+        (user === "rock" && computer === "scissors") ||
+        (user === "paper" && computer === "rock") ||
+        (user === "scissors" && computer === "paper")
+    ) {
+        return "You win!";
+    } else {
+        return "You lose!";
+    }
+}
+
+let playRound = (playerSelection) => {
+    playerSelection = playerSelection.toLowerCase();
+    let computerSelection = getComputerChoice().toLowerCase();
+    const result = determineWinner(playerSelection, computerSelection);
+    currRound.textContent = `Current Round Result: ${result}`;
+    if (computerScore == 5 || playerScore == 5){
+        displayFinalRes();
+    }
+    if (result == "You win!" && playerScore < 5){
+        playerScore += 1
+        PScore.textContent = `Score: ${playerScore}`
+    }
+    else if (result == "You lose!" && computerScore < 5){
+        computerScore += 1
+        CScore.textContent = `Score: ${computerScore}`
+    }
+  }
+
+let displayFinalRes = () => {
+    disableButtons();
     
-    if (playerSelection == `rock`){
-        if (computerSelection == `paper`){
-            return `You lose! Paper beats Rock`
-        }
-        else if (computerSelection == `scissors`){
-            return `You Win! Rock beats Scissors`
-        }
-        else return `Tie`
+    if (playerScore == 5){
+        finalRes.textContent = `Final result: Congratulation! You won!`
     }
-    else if (playerSelection == `paper`){
-        if (computerSelection == `scissors`){
-            return `You lose! Scissors beats Paper`
-        }
-        else if (computerSelection == `rock`){
-            return `You Win! Paper beats Rock`
-        }
-        else return `Tie`
+    else{
+        finalRes.textContent = `Final result: Hard Luck! You lose!`
     }
-    else if (playerSelection == `scissors`){
-        if (computerSelection == `rock`){
-            return `You lose! Rock beats Scissors`
-        }
-        else if (computerSelection == `paper`){
-            return `You Win! Scissors beats Paper`
-        }
-        else return `Tie`
-    }
+}
 
-  }
-
-  let game = () => {
-    for (let i = 0; i < 5; i++) {
-        let playerSelect = prompt("Make selection (Rock, Paper or Scissor): ", 'Rock');
-        let computerSelect = getComputerChoice();
-        let result = playRound(playerSelect, computerSelect)
-        console.log(result);
-      }
-  }
-   
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(computerSelection);
-// console.log(playRound(playerSelection, computerSelection));
-
-
-game()
+// Event Listeners
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerSelection = button.className;
+        playRound(playerSelection);
+        });
+    });
